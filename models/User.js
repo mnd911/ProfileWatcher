1c4e80ee311f2aa241185c8b6d8e3de351198346
mnd911 <m.4h910@gmail.com>
2025-05-10 15:10:35 +0300

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const xUserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('XUser', xUserSchema);
